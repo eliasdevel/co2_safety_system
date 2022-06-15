b7a674d2a55fd0c04c72ea9f26a174bec24d38dc
Elias Müller <elias.muller1@gmail.com>
2022-06-15 13:23:33 -0300

    feat: fix without interrupt

--- co2_measure.ino
+++ co2_measure.ino
@@ -5,7 +5,6 @@
 #include "Reader.h"
 #include "IoControl.h"
 #include "EepromHelper.h"
-
 // #include <EEPROM.h>
 
 const long intervalBuzzerAlarm = 500;
@@ -16,8 +15,7 @@ int minutesAux = 0;
 
 byte eeprom_time_positions[4] = {1, 3, 5, 7};
 byte eeprom_temperature_positions[4] = {0, 2, 4, 6};
-volatile int ppm = 0;
-volatile int count = 0;
+
 void setup()
 {
 
@@ -28,21 +26,12 @@ void setup()
   co2SensorSetup();
   mainMenuDisplay();
   delay(1000);
-
-  timersSetup(); // allows interrupts
 }
 
+int count = 0;
 void loop()
 {
-  if (btnPush != 'N')
-  {
-    count = 0;
-  }
-}
-
-ISR(TIMER0_COMPA_vect)
-{ // timer0 interrupt 2kHz
-
+  int ppm = (int)readCo2();
   lcd.noBlink();
   btnPush = readKeypad();
 
@@ -65,26 +54,14 @@ ISR(TIMER0_COMPA_vect)
     waitBtnRelease();
   }
 
-  if (count >= 8000 && btnPush == 'N')
+  if (count >= 20)
   {
-    count = 7800;
+    count = 19;
     lcd.clear();
     displayCo2(ppm);
   }
-}
 
-ISR(TIMER1_COMPA_vect)
-{ // timer1 interrupt 1Hz
-  // generates pulse wave of frequency 1Hz/2 = 0.5kHz (takes two cycles for full wave- toggle high then toggle low)
-  ppm = (int)readCo2();
-  ioLogic(ppm, loadMaxPPM());
- 
-}
-
-ISR(TIMER2_COMPA_vect)
-{ // timer1 interrupt 8kHz
-  // generates pulse wave of frequency 8kHz/2 = 4kHz (takes two cycles for full wave- toggle high then toggle low)
-   if (btnPush != 'N')
+  if (btnPush != 'N')
   {
     count = 0;
   }
@@ -92,56 +69,12 @@ ISR(TIMER2_COMPA_vect)
   {
     count++;
   }
-}
+  delay(200);
 
+  ioLogic(ppm, loadMaxPPM());
+}
 //--------------- End of loop() loop ---------------------
 void config()
 {
   insideMenu(1);
-}
-
-void timersSetup()
-{
-  cli(); // stop interrupts
-
-  // set timer0 interrupt at 2kHz
-  TCCR0A = 0; // set entire TCCR0A register to 0
-  TCCR0B = 0; // same for TCCR0B
-  TCNT0 = 0;  // initialize counter value to 0
-  // set compare match register for 2khz increments
-  OCR0A = 124; // = (16*10^6) / (2000*64) - 1 (must be <256)
-  // turn on CTC mode
-  TCCR0A |= (1 << WGM01);
-  // Set CS01 and CS00 bits for 64 prescaler
-  TCCR0B |= (1 << CS01) | (1 << CS00);
-  // enable timer compare interrupt
-  TIMSK0 |= (1 << OCIE0A);
-
-  // set timer1 interrupt at 1Hz
-  TCCR1A = 0; // set entire TCCR1A register to 0
-  TCCR1B = 0; // same for TCCR1B
-  TCNT1 = 0;  // initialize counter value to 0
-  // set compare match register for 1hz increments
-  OCR1A = 15624; // = (16*10^6) / (1*1024) - 1 (must be <65536)
-  // turn on CTC mode
-  TCCR1B |= (1 << WGM12);
-  // Set CS10 and CS12 bits for 1024 prescaler
-  TCCR1B |= (1 << CS12) | (1 << CS10);
-  // enable timer compare interrupt
-  TIMSK1 |= (1 << OCIE1A);
-
-  // set timer2 interrupt at 8kHz
-  TCCR2A = 0; // set entire TCCR2A register to 0
-  TCCR2B = 0; // same for TCCR2B
-  TCNT2 = 0;  // initialize counter value to 0
-  // set compare match register for 8khz increments
-  OCR2A = 249; // = (16*10^6) / (8000*8) - 1 (must be <256)
-  // turn on CTC mode
-  TCCR2A |= (1 << WGM21);
-  // Set CS21 bit for 8 prescaler
-  TCCR2B |= (1 << CS21);
-  // enable timer compare interrupt
-  TIMSK2 |= (1 << OCIE2A);
-
-  sei(); // allow interrupts
 }
